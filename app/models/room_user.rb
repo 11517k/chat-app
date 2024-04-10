12c205afc381c486fddf85eb15c5064de0d4d649
11517k <n.kobayashi1215@gmail.com>
2024-04-10 18:22:42 +0900

    メッセージ送信機能の実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,6 @@
 class RoomUser < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
+  belongs_to :room
+  belongs_to :user
   has_many :messages
 
-  validates :name, presence: true
 end
